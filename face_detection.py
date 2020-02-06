debf1bda51189fb8504b635591423af2302d8cfb
lildocinny <lildocinny@gmail.com>
2020-02-06 17:10:11 -0500

    Update face_detection.py

--- face_detection.py
+++ face_detection.py
@@ -11,7 +11,7 @@ face_locations = face_recognition.face_locations(image)
 number_of_faces = len(face_locations)
 print("I found {} face(s) in this photograph.".format(number_of_faces))
 
-# Load the image into a Python Image Library object so that we can draw on top of it and display it
+# Load the image into a Python Image Library object so that it can draw on top of it and display it
 pil_image = PIL.Image.fromarray(image)
 
 for face_location in face_locations:
@@ -20,7 +20,7 @@ for face_location in face_locations:
     top, right, bottom, left = face_location
     print("A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}".format(top, left, bottom, right))
 
-    # Let's draw a box around the face
+    #  draw a box around the face
     draw = PIL.ImageDraw.Draw(pil_image)
     draw.rectangle([left, top, right, bottom], outline="red")
 
